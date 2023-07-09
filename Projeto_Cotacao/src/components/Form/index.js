abd8982f1ed0498832ef98c68254e5170ca92d7d
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-09 20:22:04 -0300

    logica do calculo

--- Projeto_Cotacao/src/components/Form/index.js
+++ Projeto_Cotacao/src/components/Form/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, TextInput, Text, Button} from "react-native"
+import ResultImc from "./ResultImc";
 
 export default function Form(){
+
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("Preencha o peso e a altura")
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+
+
+    function imcCalculator(){
+        return setImc((weight/(height*height)).toFixed(2))
+    }
+
+    function validationImc(){
+        if(weight != null && height != null){
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu IMC é igual a:")
+            setTextButton("Calcular Novamente")
+            return
+        }
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("Preencha o peso e a altura")
+    }
+
 
     return(
         <View>
@@ -9,19 +36,26 @@ export default function Form(){
 
              <Text>Altura</Text>
              <TextInput
+                onChangeText={setHeight}
+                value={height}
                 placeholder="Ex: 1.75"
                 keyboardType="numeric"
              />
 
              <Text>Peso</Text>
              <TextInput
+                onChangeText={setWeight}
+                value={weight}
                 placeholder="Ex: 65"
                 keyboardType="numeric"
              />
 
-            <Button title="Calcular IMC"/>
+            <Button 
+            onPress={()  => validationImc()}
+            title={textButton}/>
 
            </View>
+           <ResultImc messageResultImc={messageImc} resultImc={imc}/>
         </View>
     )
 
