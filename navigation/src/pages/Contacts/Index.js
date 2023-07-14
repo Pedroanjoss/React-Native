26b918db7eaba608389dde095a2f339d9ebb61b4
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-14 00:02:34 -0300

    params navigation

--- navigation/src/pages/Contacts/Index.js
+++ navigation/src/pages/Contacts/Index.js
@@ -4,8 +4,23 @@ import { View, Text } from "react-native";
 export default function Contacts({navigation}){
     return(
         <View style={{marginTop:60}}>
-            <Text>Joao</Text>
-            <Text onPress={() => navigation.navigate('Information')}>Information...</Text>
+           <View>
+              <Text>Nome: Joao da Silva</Text>
+              <Text>Telefone: (11) 98976-7744</Text>
+              <Text onPress={() => navigation.navigate('Information', {
+                nome: "Joao da Silva",
+                telefone:" (11) 98976-7744",
+                endereco: "Rua das Flores",
+                numero: "769",
+                profissao: "Carpinteiro",
+                email: "joaosilva@carpinteiro.com.br",
+              })}>Information...</Text>
+           </View>
+           <View  style={{marginTop:20}}>
+              <Text>Nome: Amanda da Silva</Text>
+              <Text>Telefone: (11) 94563-4477</Text>
+              <Text onPress={() => navigation.navigate('Information')}>Information...</Text>
+           </View>
         </View>
     )
 }
--- navigation/src/pages/Information/Index.js
+++ navigation/src/pages/Information/Index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Information(){
+export default function Information({route}){
     return(
         <View>
-            <Text>Information</Text>
+            <Text>Nome: {route.params?.nome}</Text>
+            <Text>Telefone: {route.params?.telefone}</Text>
+            <Text>Endereco: {route.params?.endereco}</Text>
+            <Text>n: {route.params?.numero}</Text>
+            <Text>Profissao:{route.params?.profissao}</Text>
+            <Text>email: {route.params?.email}</Text>
         </View>
     )
 }
