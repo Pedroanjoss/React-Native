b72524974c201b30bdc4dc1fc8706535735f6940
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-19 22:08:50 -0300

    plataform config

--- myTasks/src/Pages/Home/index.tsx
+++ myTasks/src/Pages/Home/index.tsx
@@ -1,23 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,SafeAreaView,Platform, Text, View, TextInput } from 'react-native';
 import React from 'react';
 
 export const  Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Ola Mundo</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+     <View style={styles.container}>
+        <Text style={styles.title}>Welcome, Dev!</Text>
+        <TextInput style={styles.input}/>
+     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea:{
+    
+      flex: 1,
+      backgroundColor: "#121214",
+  },
   container: {
     flex: 1,
     backgroundColor: "#121214",
-    justifyContent:"center",
-    alignItems: "center"
+    paddingHorizontal: 20,
+    paddingVertical: 50
+    
   },
   title:{
-    color: "#f1f1f1"
+    color: "#f1f1f1",
+    fontSize:24,
+    fontWeight: "bold"  
+  },
+  input:{
+    backgroundColor:"#29292e",
+    color: "#f1f1f1",
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 :12,
+    marginTop: 30,
+    borderRadius: 7
   }
 });
