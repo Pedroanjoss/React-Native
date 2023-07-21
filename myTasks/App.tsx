425b019f0620fd0a6d02e5f50227373e35790158
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-20 23:04:31 -0300

    criando o context

--- myTasks/App.tsx
+++ myTasks/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Home } from './src/Pages/Home';
+import { TasksContext } from './src/Context/TaskContext';
+
 
 export default function App() {
   return (
-    <Home></Home>
+    <TasksContext.Provider value={{id: '1', title: 'Task01'}}>
+        <Home></Home>
+    </TasksContext.Provider>
   );
 }
 
