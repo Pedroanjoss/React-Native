54cd438898506811355bedeb217fd07ebcaf5b24
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-23 21:12:31 -0300

    componente home

--- devProfile/App.tsx
+++ devProfile/App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import { Home } from './src/pages/Home';
+
+ const App: React.FunctionComponent = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <Home></Home>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+
