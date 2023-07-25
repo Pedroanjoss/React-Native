b96610fe991c5cc35d6ddce4090e9ee0d8327f4f
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-24 22:50:07 -0300

    tema global

--- devProfile/App.tsx
+++ devProfile/App.tsx
@@ -1,10 +1,14 @@
 
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Home } from './src/pages/Home';
+import theme from './src/global/styles/theme';
 
  const App: React.FunctionComponent = () => {
   return (
-  <Home></Home>
+    <ThemeProvider theme={theme}>
+       <Home></Home>
+    </ThemeProvider>
   );
 }
 
