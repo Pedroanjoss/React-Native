6524c9295339bccafa5d3587e5c0d7a272f3f424
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-24 23:13:40 -0300

    utilizando fontes personalizadas

--- devProfile/App.tsx
+++ devProfile/App.tsx
@@ -3,9 +3,19 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Home } from './src/pages/Home';
 import theme from './src/global/styles/theme';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto"
 
  const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,Roboto_700Bold
+  });
+  
+  if(!fontsLoaded){
+    return null
+  }
+
   return (
+    
     <ThemeProvider theme={theme}>
        <Home></Home>
     </ThemeProvider>
