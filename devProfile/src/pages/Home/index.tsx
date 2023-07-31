a2d6d8fe01d968e7e33d589c5887c4515fcf3b99
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-30 22:50:49 -0300

    estrutura de componentes

--- devProfile/src/pages/Home/index.tsx
+++ devProfile/src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { Container, Header } from './styles'
+import { Container, Header, UserWrapper, UserInfo, UserAvatarButton, UserAvatar, UserInfoDetail, UserGreeting, UserName } from './styles'
+import avatarDefault from "../../assets/avatar1.png"
+
+
+
 
 export const Home: React.FunctionComponent = () => {
     return(
         <Container>
-            <Header></Header>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <UserAvatarButton onPress={() => {}}>
+                            <UserAvatar source={avatarDefault}/>
+                        </UserAvatarButton>
+                        <UserInfoDetail>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Pedro</UserName>
+                        </UserInfoDetail>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
