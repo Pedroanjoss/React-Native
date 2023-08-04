25c081fdcf0c93afdb1c1c38d213083c65fdcb9f
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-08-03 22:10:03 -0300

    acessando a pagina de SignIn

--- devProfile/src/pages/SignIn/index.tsx
+++ devProfile/src/pages/SignIn/index.tsx
@@ -1,31 +1,41 @@
 import React from "react";
-import { Container, Content, ForgotPasswordButton, ForgotPasswordTitle, Logo, Title } from "./styles";
+import { Container, Content, CreateAccount, ForgotPasswordButton, ForgotPasswordTitle, Icon, Logo, Title, CreateAccountTitle } from "./styles";
 import { Input } from "../../components/form/input";
-import { ScrollView } from "react-native";
+import { ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 import { Button } from "../../components/form/Button";
 import logo from "../../assets/logo.png"
 
 
 export const SignIn: React.FunctionComponent = () => {
     return (
-        <ScrollView 
-          keyboardShouldPersistTaps='handled'
-          contentContainerStyle={{ flex:1 }}
-        >
-            <Container>
-                <Content>
-                    <Logo source={logo}/>
-                    <Title>Faça seu login</Title>
-                    <Input placeholder="Email"></Input>
-                    <Input placeholder="Senha"></Input>
+        <KeyboardAvoidingView enabled style={{ flex: 1}} behavior={ Platform.OS === 'ios' ? 'padding': undefined }>
+            <ScrollView 
+            keyboardShouldPersistTaps='handled'
+            contentContainerStyle={{ flex:1 }}
+            >
+                <Container>
+                    <Content>
+                        <Logo source={logo}/>
+                        <View>
+                          <Title>Faça seu login</Title>
+                        </View>
+                        <Input placeholder="Email"></Input>
+                        <Input placeholder="Senha"></Input>
 
-                    <Button title="Entrar"/>
+                        <Button title="Entrar"/>
 
-                    <ForgotPasswordButton>
-                        <ForgotPasswordTitle>Esqueci minha senha </ForgotPasswordTitle>
-                    </ForgotPasswordButton>
-                </Content>
-            </Container>
-        </ScrollView>
+                        <ForgotPasswordButton>
+                            <ForgotPasswordTitle>Esqueci minha senha </ForgotPasswordTitle>
+                        </ForgotPasswordButton>
+                    </Content>
+                </Container>
+            </ScrollView>
+
+            <CreateAccount>
+                <Icon name="log-in"/>
+                <CreateAccountTitle>Criar uma conta</CreateAccountTitle>
+            </CreateAccount>
+
+        </KeyboardAvoidingView>
     )
 }
