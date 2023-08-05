484442d785782b2307b4b6195802fff60dde4e96
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-08-04 23:20:23 -0300

    implementando inputControl

--- devProfile/src/pages/SignIn/index.tsx
+++ devProfile/src/pages/SignIn/index.tsx
@@ -2,18 +2,35 @@ import React from "react";
 import { Container, Content, CreateAccount, ForgotPasswordButton, ForgotPasswordTitle, Icon, Logo, Title, CreateAccountTitle } from "./styles";
 import { Input } from "../../components/form/input";
 import { ScrollView, KeyboardAvoidingView, Platform, View } from "react-native";
+import { useForm, FieldValues} from "react-hook-form";
 import { Button } from "../../components/form/Button";
 import logo from "../../assets/logo.png"
 import { useNavigation } from "@react-navigation/native";
+import { InputControl } from "../../components/form/inputControl";
 
 
 interface ScreenNavigationProp {
     navigate: (screen: string) => void
 }
 
+interface IformInputs{
+    [name: string]: any
+}
+
 export const SignIn: React.FunctionComponent = () => {
+    const {handleSubmit, control} = useForm<FieldValues>()
+
 
     const {navigate} = useNavigation<ScreenNavigationProp>()
+
+    const handleSignIn = (form: IformInputs) => {
+        const data = {
+            email: form.email,
+            password: form.password,
+        };
+
+        console.log(data)
+    }
 
     return (
         <KeyboardAvoidingView enabled style={{ flex: 1}} behavior={ Platform.OS === 'ios' ? 'padding': undefined }>
@@ -27,10 +44,10 @@ export const SignIn: React.FunctionComponent = () => {
                         <View>
                           <Title>Faça seu login</Title>
                         </View>
-                        <Input placeholder="Email"></Input>
-                        <Input placeholder="Senha"></Input>
+                        <InputControl name="email" autoCapitalize="none" autoCorrect={false} keyboardType="email-address" control={control}  placeholder="Email"></InputControl>
+                        <InputControl name="password" autoCapitalize="none" autoCorrect={false} control={control} placeholder="Senha" secureTextEntry></InputControl>
 
-                        <Button title="Entrar"/>
+                        <Button title="Entrar" onPress={() => handleSubmit(handleSignIn)}/>
 
                         <ForgotPasswordButton>
                             <ForgotPasswordTitle>Esqueci minha senha </ForgotPasswordTitle>
